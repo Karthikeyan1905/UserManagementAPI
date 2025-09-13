089b6bf495d116c8e4958b7e23c0105e33e193cc
Your Name <your-email@example.com>
2025-09-13 23:14:34 +0530

    Add project files.

---
+++ Controllers/UserInfoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagement.Bussiness.Repository;
+using UserManagement.Bussiness.RepositoryInterface;
+using UserManagement.Model.User;
+
+namespace UserManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserInfoController : Controller
+    {
+        
+        [HttpGet(Name = "GetSelect")]
+        public IEnumerable<UserInfo> GetSelect([FromQuery] int userid, [FromQuery] string status )
+        {
+           
+            IUserInfoRepository _repository = new UserInfoRepository();
+           return _repository.Select(new UserInfo() { userID = userid, Status = status});
+
+        }
+    }
+}
